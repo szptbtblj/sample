7393155e6009e59e7284697e68e82b9db313a82f
sasa <sasaki.kh.530@gmail.com>
2018-10-25 15:20:31 +0900

    sample
    sample

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>卒業研究2018</title>
+<style>
+body{
+background: #F5DEB3;
+}
+center{
+text-align:center;
+}
+left{
+text-align:left;
+}
+button{
+color:black;
+font-size:20px;
+}
+</style>
+</head>
+<body>
+<center>
+<h1>位置情報型ARとピクトグラムを利用した観光支援システム</h1></br>
+</center>
+<a href="https://arlocation.herokuapp.com"><img src="home.png" style="width:100px;height:30px;" alt="home"></a>　
+<a href="https://arlocation.herokuapp.com/regist.html"><img src="user_regist.png" style="width:130px;height:30px;" alt="regist"></a>　
+<!--
+<a href="a.html"><img src="question.png" style="width:120px;height:30px;" alt="question"></a>　
+<a href="view.php"><img src="view.png" style="width:100px;height:30px;" alt="view"></a>　
+-->
+<!--
+<button type="button" style="border-width:4;width:100px;height:50px;background:#F5DEB3;cursor:pointer" onclick="location.href='https://arlocation.herokuapp.com/index.php'">ホーム</button>
+<button type="button" style="border-width:4;width:130px;height:50px;background:#F5DEB3;cursor:pointer" onclick="location.href='https://arlocation.herokuapp.com/regist.html'">利用者登録</button>
+<button type="button" style="border-width:4;width:130px;height:50px;background:#F5DEB3;cursor:pointer" onclick="location.href='a.html'">アンケート</button>
+<button type="button" style="border-width:4;width:100px;height:50px;background:#F5DEB3;cursor:pointer" onclick="location.href='https://arlocation.herokuapp.com/view.php'">掲示板</button>
+-->
+<!--<button type="button" style="border-width:4;width:140px;height:50px;background:#F5DEB3;cursor:pointer" onclick="location.href='a.html'">お問い合わせ</button>-->
+<h3><概要></h3>
+<h4>
+位置情報型ARを用いて, 調布市観光マップで紹介されている観光名所をARで表示したAndroidアプリです. <br>ご利用の際は以下のインストールの手順と注意事項をお読みください.
+</h4>
+<h3><インストールの手順></h3>
+<h4>
+①以下のURLをクリックして利用者登録を行います.<br>
+<a href="https://arlocation.herokuapp.com/regist.html">利用者登録</a><br>
+②登録が確認された後, 登録されたメールアドレスにメールが送信されます.<br>
+③表示されるURLをクリックしてGoogle Driveにアクセスします.<br>
+④Drive上のapkファイルをインストールすることで, アプリケーションをインストールします.<br><br>
+ご利用後はこちらのアンケートへのご協力をお願いします.<br>
+<a href="b.html">アンケートへ</a>
+</h4>
+<br>
+<h3><注意事項></h3>
+<h4>
+・インストールの手順④でご自身の端末の「提供元不明のアプリ」許可設定を行う必要があります. インストール後は必ず無効にしてください.<br>
+・対応端末はAndroid5.0以降のものとなります.<br>
+・ご利用の際, Googleアカウントが必要になります.
+</h4>
+<h4>
+参考画像1
+</h4>
+<img src="sample1.png" width="413px" height="232px">
+<br>
+<h4>
+参考画像2
+</h4>
+<img src="sample2.png" width="413px" height="232px">
+<br><br>
+<!--
+<center><a href="a.html">掲示板</a></center>
+-->
+</body>
+</html>
